d4f7cf878060bf3d7885347abfeccd192ba55b84
Enrique J. Hernández <enrique@heetch.com>
2020-06-30 15:12:18 +0200

    Add avrotest package to ease integration testing

---
+++ avrotest/avrotest.go
@@ -0,0 +1,58 @@
+package avrotest
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/heetch/avro"
+	"github.com/heetch/avro/avroregistry"
+)
+
+// Register an avro type in a registry for a particular topic and then delete the subject at the end of the test.
+//
+// The following environment variables can be used to
+// configure the connection parameters:
+//
+//	- $KAFKA_REGISTRY_ADDR
+//		The Kafka registry address in host:port
+//		form. If this is empty, localhost:8084 will be used.
+//
+func Register(ctx context.Context, t T, x interface{}, topic string) error {
+	registryAddr := os.Getenv("KAFKA_REGISTRY_ADDR")
+	if registryAddr == "" {
+		registryAddr = "localhost:8084"
+	}
+
+	registry, err := avroregistry.New(avroregistry.Params{
+		ServerURL: "http://" + registryAddr,
+	})
+	if err != nil {
+		return fmt.Errorf("cannot connect to registry: %w", err)
+	}
+
+	avroType, err := avro.TypeOf(x)
+	if err != nil {
+		return fmt.Errorf("cannot generate Avro schema for %T: %w", x, err)
+	}
+
+	_, err = registry.Register(ctx, topic, avroType)
+	if err != nil {
+		return fmt.Errorf("cannot register %T in %v: %w", x, topic, err)
+	}
+
+	t.Cleanup(func() {
+		err := registry.DeleteSubject(ctx, topic)
+		if err != nil {
+			t.Errorf("cannot delete subject: %w", err)
+		}
+	})
+
+	return nil
+}
+
+// T represents a test (the usual implementation being *testing.T).
+type T interface {
+	Cleanup(f func())
+	Errorf(f string, a ...interface{})
+}
---
+++ avrotest/avrotest_test.go
@@ -0,0 +1,45 @@
+package avrotest
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+type x struct {
+	Int int
+	Str string
+}
+
+func TestRegister(t *testing.T) {
+	c := qt.New(t)
+
+	c.Run("OK", func(c *qt.C) {
+		err := Register(context.Background(), c, x{}, randomName("test-"))
+		c.Assert(err, qt.IsNil)
+	})
+
+	c.Run("NOK - Wrong interface", func(c *qt.C) {
+		err := Register(context.Background(), c, struct{}{}, randomName("test-"))
+		c.Assert(err, qt.Not(qt.IsNil))
+		c.Assert(err, qt.ErrorMatches, "cannot generate Avro schema for.*")
+	})
+
+	c.Run("NOK - Wrong addr", func(c *qt.C) {
+		c.Setenv("KAFKA_REGISTRY_ADDR", "-host:1234")
+		err := Register(context.Background(), c, x{}, randomName("test-"))
+		c.Assert(err, qt.Not(qt.IsNil))
+		c.Assert(err, qt.ErrorMatches, "cannot register.*")
+	})
+}
+
+func randomName(prefix string) string {
+	buf := make([]byte, 8)
+	if _, err := rand.Read(buf); err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s%x", prefix, buf)
+}
